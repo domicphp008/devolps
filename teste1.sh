a2a6506cbe6be76db034ceefeac709ac2f91408b
Domicio <testedomicio@hotmail.com>
2023-11-24 16:59:33 -0300

    devolps
    script básico que apenas exibe uma mensagem na tela.
    Exemplo clonado.

---
+++ teste1.sh
@@ -0,0 +1,27 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Bold;\f1\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red254\green209\blue10;\red32\green32\blue32;\red246\green246\blue239;
+\red202\green199\blue155;\red157\green225\blue43;}
+{\*\expandedcolortbl;;\cssrgb\c100000\c84314\c0;\cssrgb\c16863\c16863\c16863;\cssrgb\c97255\c97255\c94902;
+\cssrgb\c83137\c81569\c67059;\cssrgb\c67059\c89020\c21961;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\b\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 #!/bin/sh  
+\f1\b0 \cf4 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf5 \strokec5 # a linha acima declara o bin que executar\'e1 o script\cf4 \strokec4 \
+          \
+\pard\pardeftab720\partightenfactor0
+\cf2 \strokec2 clear\cf4 \strokec4                 \
+\pard\pardeftab720\partightenfactor0
+\cf5 \strokec5 # a linha acima executa um comando shell que limpa a tela\cf4 \strokec4 \
+\
+\pard\pardeftab720\partightenfactor0
+\cf6 \strokec6 echo\cf4 \strokec4  \cf6 \strokec6 "Ol\'e1 mundo!"\cf4 \strokec4     \
+\pard\pardeftab720\partightenfactor0
+\cf5 \strokec5 # a linha acima escreve a mensagem "Ol\'e1 mundo"\cf4 \strokec4 \
+\
+\cf5 \strokec5 # coment\'e1rios s\'e3o feitos com '#'}
